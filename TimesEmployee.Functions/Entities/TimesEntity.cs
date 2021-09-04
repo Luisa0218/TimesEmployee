86673ca1077cf4376cf10d732971e9ca503b8a1d
LUISA FERNANDA VERGARA SEPULVEDA <fernandavergara0218@gmail.com>
2021-09-04 15:59:36 -0500

    Add Entity

---
+++ TimesEmployee.Functions/Entities/TimesEntity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace TimesEmployee.Functions.Entities
+{
+    public class TimesEntity : TableEntity
+    {
+        public int IdEmployee { get; set; }
+
+        public DateTime DateHour { get; set; }
+
+        public int Type { get; set; }
+
+        public bool Consolidate { get; set; }
+
+    }
+}
