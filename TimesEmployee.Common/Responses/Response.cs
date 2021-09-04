d1b8e97b0d81e55312a237416e561ad1db3e299b
LUISA FERNANDA VERGARA SEPULVEDA <fernandavergara0218@gmail.com>
2021-09-04 15:35:39 -0500

    Add  Class Response

---
+++ TimesEmployee.Common/Responses/Response.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TimesEmployee.Common.Responses
+{
+    public class Response
+    {
+        public int IdEmployee { get; set; }
+
+        public DateTime DateHour { get; set; }
+
+        public string Message { get; set; }
+
+        public object Result { get; set; }
+    }
+}
+
