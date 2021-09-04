ece424756fd8bbc7126c3b6e166c70769bb2c322
LUISA FERNANDA VERGARA SEPULVEDA <fernandavergara0218@gmail.com>
2021-09-04 15:30:45 -0500

    Add Class Times

---
+++ TimesEmployee.Common/Models/Times.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TimesEmployee.Common.Models
+{
+    public class Tasks
+    {
+        public int IdEmployee { get; set; }
+
+        public DateTime DateHour { get; set; }
+
+        public int Type { get; set; }
+
+        public bool Consolidate { get; set; }
+
+    }
+}
